a1b187b4e2d14313450cfc134ed25e557aedf901
taral-tagline <taral.tagline@gmail.com>
2022-06-09 12:17:55 +0530

    upload image on spacific folder

--- facerecoproj/main.py
+++ facerecoproj/main.py
@@ -2,51 +2,44 @@ from flask import Flask, render_template, Response,request,redirect,flash,url_fo
 from .facereco import gen_frames
 import os
 from pathlib import Path
-import urllib.request
-from werkzeug.utils import secure_filename
 
-PEOPLE_FOLDER = os.path.join('static', 'capture_image')
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+PEOPLE_FOLDER = os.path.join(Path(__file__).resolve().parent, 'static/capture_image')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route("/")
 def index():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Frame0.jpg')
-    return render_template("index.html", user_image = full_filename)
+    static_url = "capture_image/taral.jpeg"
+    return render_template("index.html", static_url = static_url)
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/upload")
+@app.route("/upload",methods = ['POST','GET'])
 def upload():
-    return render_template("upload.html")
+    static_url = "capture_image/taral.jpeg"
+    if request.method == 'POST':  
+        uname=request.form['input_name']
+        upload_file = request.files['input_file']
+        filename = upload_file.filename
+        # # Store Form Data into table
+        # upload = Upload(name=uname , data = upload_file.read())
+        # db.session.add(upload)
+        # db.session.commit()
 
-@app.route('/', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('upload.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
+        # Store image in folder
+        upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # upload_image = cv2.imread(PEOPLE_FOLDER + upload_file.filename)
+        simple_msg = f'File Uploaded name : {filename} and your name is %s' %uname
+
+        return render_template('upload.html',simple_msg = simple_msg, static_url = static_url)
+    else:
+        return render_template('upload.html', static_url = static_url)
 
 @app.route('/display/<filename>')
-def display_image(filename):
+def display_image():
 	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+	return redirect(url_for('static', filename='capture_image/taral.jpeg'), code=301)
