a621256eee92136fff95e6a9efb53df16936565c
taral-tagline <taral.tagline@gmail.com>
2022-06-09 09:35:13 +0530

    add upload image page and capture image from webcam if person face detect on webcam

--- facerecoproj/main.py
+++ facerecoproj/main.py
@@ -1,86 +1,52 @@
-from flask import Flask, render_template, Response
-import face_recognition
-import cv2
-import numpy as np
+from flask import Flask, render_template, Response,request,redirect,flash,url_for
+from .facereco import gen_frames
+import os
+from pathlib import Path
+import urllib.request
+from werkzeug.utils import secure_filename
 
+PEOPLE_FOLDER = os.path.join('static', 'capture_image')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-camera = cv2.VideoCapture("/Users/tagline/Desktop/project/flask_img_reco/facerecoproj/taral/face_video.mp4")
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
-my_image = face_recognition.load_image_file("/Users/tagline/Desktop/project/flask_img_reco/facerecoproj/taral/taral.jpeg")
-my_encodings = face_recognition.face_encodings(my_image)[0]
-
-known_face_encodings = [my_encodings]
-known_face_names = ["taral"]
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-def gen_frames():  
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            # Resize frame of video to 1/4 size for faster face recognition process
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-            # convert image from BGR (which OpenCV uses) to Rgb Color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-
-            # only process every other frame of video of save time
-            if process_this_frame:
-                # find all the face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-                face_names = []
-                for face_encoding in face_encodings :
-                    # see if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
-
-                    # or instead, use the known face with the smallest distence
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index] :
-                        name = known_face_names[best_match_index]
-                    
-                    face_names.append(name)
-
-            # display the results
-            for (top, right, bottom, left), name in zip(face_locations,face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                # Draw a box arround the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left,bottom - 35),(right,bottom),(0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-    
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Frame0.jpg')
+    return render_template("index.html", user_image = full_filename)
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/privacy-policy")
-def privacy_policy():
-    return render_template("privacy_policy.html")
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
